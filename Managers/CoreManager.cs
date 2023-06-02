ba249b2f2851c705a10a541de2bccd30e6a25505
ChocolateAdventurouz <92590193+ChocolateAdventurouz@users.noreply.github.com>
2023-06-02 16:50:47 +0300

    added a core manager @georgesepetadelis

---
+++ Managers/CoreManager.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Filename: Managers.cs
+    Managers.cs is a part of Linuxide. 
+    Used for method declaration 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linuxide
+{
+    internal class CoreManager
+    {
+    }
+}
