422c056bb5339f2560c0959c65c7ff079482d3eb
ChocolateAdventurouz <92590193+ChocolateAdventurouz@users.noreply.github.com>
2023-06-03 19:01:46 +0300

    cleared useless code

--- Preperation.cs
+++ Preperation.cs
@@ -78,42 +78,6 @@ namespace Linuxide
              */
 
 
-            //nextButton = parentForm.button1;
-            //nextButton.Text = string.Empty;
-
-            DiskPart dp = new DiskPart();
-            //dp.ShrinkPartition();
-
-
-            listBox1.Items.Clear(); // took me a while to realize that i need this
-
-
-            // windows API provides me the fucking solution in partition managment, and i was making my own one, oh well, 3 days wasted.
-            ManagementObjectSearcher win32DiskPartitions = new ManagementObjectSearcher("select * from Win32_DiskPartition");
-            foreach (ManagementObject win32DiskPartition in win32DiskPartitions.Get())
-            {
-                partcount++;
-                Fullpart[partcount] = win32DiskPartition["Name"].ToString();
-                partNums[partcount] = win32DiskPartition["Index"].ToString();
-                diskNums[partcount] = win32DiskPartition["DiskIndex"].ToString();
-
-                //string part[] = win32DiskPartition["Name"].ToString();
-                listBox1.Items.Add(Fullpart[partcount]);
-                partitionList.Add(partNums[partcount]);
-                diskList.Add(diskNums[partcount]);
-                //Console.WriteLine("Partition found:\n{0}",win32DiskPartition["Name"]);
-                Console.WriteLine("Partition nums all: {0}", Fullpart[partcount]);
-
-            }
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            selectedValue = listBox1.SelectedIndex;
-
-            Console.WriteLine("|Disk Selected: {0}", diskList[selectedValue]);
-            Console.WriteLine(" ->Partition of Disk Selected: {0}", partitionList[selectedValue]);
-        }
+         }
     }
 }
