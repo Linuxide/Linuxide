4a80d18dfd4ed88a27b5a6ba8092995919911284
ChocolateAdventurouz <92590193+ChocolateAdventurouz@users.noreply.github.com>
2023-06-07 22:45:51 +0300

    added MainWindow controlling.

--- Managers/CoreManager.cs
+++ Managers/CoreManager.cs
@@ -9,10 +9,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Linuxide
 {
-    internal class CoreManager
+    public class Core
     {
+        public void initMainWindow_Control() // controlling MainWindow controls
+        {
+            var mainWindow = (Application.OpenForms.OfType<MainWindow>().FirstOrDefault() ?? new MainWindow());
+            return;
+        }
+        public void letMainWindow_Control() // erasing f contents
+        {
+            var mainWindow = String.Empty;
+            return;
+        }
     }
 }
